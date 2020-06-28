f230062ab59f74e0755119b7823f9f983dea169e
Rodolfo De Nadai <rdenadai@gmail.com>
2020-06-28 18:43:57 -0300

    Ajustes

--- notebooks/Python201/src/python201_1_pdb.py
+++ notebooks/Python201/src/python201_1_pdb.py
@@ -4,18 +4,19 @@ import pdb
 def debugger_old_way(x, y):
     z = x + y
     pdb.set_trace()
-    z = z**2
+    z = z ** 2
     return z
 
 
 def debugger_new_way(x, y):
     z = x + y
     breakpoint()
-    z = z**2
+    z = z ** 2
     return z
 
 
 # debugger_old_way(10, 2)
 
-debugger_new_way(10, 2)
-
+if __name__ == "__main__":
+    print("here")
+    debugger_new_way(10, 2)
